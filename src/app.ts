0bcc35666af83209f8682d6a51563f67fd7dffe2
marufkhan20 <mdm326040@gmail.com>
2026-08-18 02:30:24 +0600

    feat: add http-errors dependency and implement global error handling middleware

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,34 @@
-import express from "express";
+import express, {
+  type NextFunction,
+  type Request,
+  type Response,
+} from "express";
+import logger from "./config/logger.ts";
+import type { HttpError } from "http-errors";
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", async (_req, res) => {
   res.send("Welcome To Auth Service.");
+});
+
+// global error handler
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+  logger.error(err.message);
+
+  const statusCode = err.statusCode || 500;
+
+  res.status(statusCode).json({
+    errors: [
+      {
+        type: err.name,
+        msg: err.message,
+        path: "",
+        location: "",
+      },
+    ],
+  });
 });
 
 export default app;
