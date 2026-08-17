6ada6633e0a6edbb6fbb2410f433e10ab603c85f
marufkhan20 <mdm326040@gmail.com>
2026-08-17 19:00:40 +0600

    feat: implement Winston logging service and integrate it into the application server

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import app from "./app.ts";
 import { Config } from "./config/index.ts";
+import logger from "./config/logger.ts";
 
 const startServer = () => {
   const PORT = Config.PORT;
   try {
-    app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+    app.listen(PORT, () => logger.info("Listening on PORT", { port: PORT }));
   } catch (error) {
     console.error(error);
     process.exit(1);
