4db1a0f694946c322c1e3c645519d82fc6dd8d49
marufkhan20 <mdm326040@gmail.com>
2026-08-17 14:36:55 +0600

    setup eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+
+import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+
+export default defineConfig(
+  {
+    files: ["**/*.{js,ts}"],
+    extends: [js.configs.recommended, tseslint.configs.recommended],
+  },
+  { ignores: ["dist", "node_modules"] },
+);
